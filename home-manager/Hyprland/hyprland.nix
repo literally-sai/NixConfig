44a92eaae7839c77bbcdfc5cf0ce2485d8d6f37d
literally-sai <173792483+literally-sai@users.noreply.github.com>
2025-03-19 13:24:28 +0100

    update

--- home-manager/Hyprland/hyprland.nix
+++ home-manager/Hyprland/hyprland.nix
@@ -72,45 +72,47 @@
         };
       };
 
-      bind = [
-        "$MOD, Q, exec, $terminal"
-        "$MOD, C, killactive,"
-        "$MOD, W, exec, $terminal -e nvim"
-        "$MOD, V, fullscreen,"              # Fullscreen with MOD V
-        "$MOD ALT, V, togglefloating,"      # Floating with MOD Alt V
-        "$MOD SHIFT, V, exec, pypr toggle term && hyprctl dispatch bringactivetotop"  # Toggle scratchpad with MOD Shift V
-        "$MOD SHIFT, L, exec, hyprlock"     # Lock with MOD Shift L
-        "$MOD, E, exec, $menu"
-        "$MOD, R, exec, $cliFM"
-        "$MOD, Shift Space, togglesplit,"
-        "$MOD, H, movefocus, l"
-        "$MOD, L, movefocus, r"
-        "$MOD, J, movefocus, d"
-        "$MOD, K, movefocus, u"
-        "$MOD Shift, H, movewindow, l"
-        "$MOD Shift, L, movewindow, r"
-        "$MOD Shift, J, movewindow, d"
-        "$MOD Shift, K, movewindow, u"
-        "$MOD ALT, H, resizeactive, 20 0"
-        "$MOD ALT, L, resizeactive, -20 0"
-        "$MOD ALT, J, resizeactive, 0 -20"
-        "$MOD ALT, K, resizeactive, 0 20"
-        "$MOD, P, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 20%+"
-        "$MOD, O, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 20%-"
-        "$MOD SHIFT, P, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 100%"
-        "$MOD SHIFT, O, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%"
-      ] ++ (builtins.concatLists (
-        builtins.genList (
-          i:
-          let
-            ws = i + 1;
-          in
-          [
-            "$MOD, code:1${toString i}, workspace, ${toString ws}"
-            "$MOD SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-          ]
-        ) 9
-      ));
+      bind =
+        [
+          "$MOD, Q, exec, $terminal"
+          "$MOD, C, killactive,"
+          "$MOD, W, exec, $terminal -e nvim"
+          "$MOD, V, fullscreen," # Fullscreen with MOD V
+          "$MOD ALT, V, togglefloating," # Floating with MOD Alt V
+          "$MOD SHIFT, V, exec, pypr toggle term && hyprctl dispatch bringactivetotop" # Toggle scratchpad with MOD Shift V
+          "$MOD SHIFT, U, exec, hyprlock" # Lock with MOD Shift L
+          "$MOD, E, exec, $menu"
+          "$MOD, R, exec, $cliFM"
+          "$MOD, Shift Space, togglesplit,"
+          "$MOD, H, movefocus, l"
+          "$MOD, L, movefocus, r"
+          "$MOD, J, movefocus, d"
+          "$MOD, K, movefocus, u"
+          "$MOD Shift, H, movewindow, l"
+          "$MOD Shift, L, movewindow, r"
+          "$MOD Shift, J, movewindow, d"
+          "$MOD Shift, K, movewindow, u"
+          "$MOD ALT, H, resizeactive, 20 0"
+          "$MOD ALT, L, resizeactive, -20 0"
+          "$MOD ALT, J, resizeactive, 0 -20"
+          "$MOD ALT, K, resizeactive, 0 20"
+          "$MOD, P, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 20%+"
+          "$MOD, O, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 20%-"
+          "$MOD SHIFT, P, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 100%"
+          "$MOD SHIFT, O, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%"
+        ]
+        ++ (builtins.concatLists (
+          builtins.genList (
+            i:
+            let
+              ws = i + 1;
+            in
+            [
+              "$MOD, code:1${toString i}, workspace, ${toString ws}"
+              "$MOD SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          ) 9
+        ));
 
       bindm = [
         "$MOD, mouse:272, movewindow"
@@ -119,3 +121,4 @@
     };
   };
 }
+
